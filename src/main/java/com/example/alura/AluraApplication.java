b09e33c7e832e0cf28c459715b8178610e378616
laryscristine <laryssa.crisdias@gmail.com>
2021-04-11 14:04:17 -0300

    Finalizacao dos testes Java

--- src/main/java/com/example/alura/AluraApplication.java
+++ src/main/java/com/example/alura/AluraApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
+//BUILD IMAGE ON DOCKER:
+//docker build -r /alura/forum .
+//docker image list - > Lista as imagens do docker
 @SpringBootApplication
 @EnableSpringDataWebSupport
 @EnableCaching
